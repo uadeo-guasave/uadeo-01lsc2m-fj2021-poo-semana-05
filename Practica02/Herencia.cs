a56036a010868a7e222902498abce66fafc886d7
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2021-02-26 13:02:17 -0700

    Herencia
    Sobreescritur del método ToString

--- Practica02/Herencia.cs
+++ Practica02/Herencia.cs
@@ -6,11 +6,56 @@
  * Mouse
  * Disco
  **/
+using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var procIntel = new Procesador()
+        {
+            Marca = "Intel",
+            Modelo = "Core i5 11898",
+            TipoDeComponente = "Procesador",
+            Generación = "11va",
+            Socket = "1200",
+            Núcleos = 4,
+            Velocidad = 2.4f
+        };
+
+        var ramKingston = new Ram()
+        {
+            Marca = "Kingston",
+            Modelo = "DDR4",
+            TipoDeComponente = "Memoria RAM",
+            TipoDeMemoria = "DIMM",
+            Capacidad = 8,
+            Velocidad = 1200
+        };
+
+        Console.WriteLine("Imprimiendo procIntel");
+        Console.WriteLine(procIntel);
+        Console.WriteLine("Imprimiendo ramKingston");
+        Console.WriteLine(ramKingston);
+
+        Console.WriteLine("Usando método imprimir");
+        Console.WriteLine(procIntel.ImprimirProcesador());
+        Console.WriteLine(ramKingston.ImprimirRam());
+    }
+}
 
 class ComponenteDeComputadora
 {
     public string Marca { get; set; }
     public string Modelo { get; set; }
+    public string TipoDeComponente { get; set; }
+
+    public override string ToString()
+    {
+        return $"{TipoDeComponente} {Marca}, Modelo {Modelo}";
+    }
+
+    // sobrecarga de métodos (overload)
+    // sobreescritura de métodos (override)
 }
 
 class Procesador : ComponenteDeComputadora
@@ -28,7 +73,7 @@ class Procesador : ComponenteDeComputadora
 
 class Ram : ComponenteDeComputadora
 {
-    public string Tipo { get; set; }
+    public string TipoDeMemoria { get; set; }
     public int Capacidad { get; set; }
     public int Velocidad { get; set; }
 
@@ -41,17 +86,17 @@ class Ram : ComponenteDeComputadora
 class Monitor : ComponenteDeComputadora
 {
     public int Dimensión { get; set; }
-    public string Tipo { get; set; }
+    public string TipoDeMonitor { get; set; }
 
     public string ImprimirMonitor()
     {
-        return $"Monitor {Marca} Modelo {Modelo} Dimensión {Dimensión} Tipo {Tipo}";
+        return $"Monitor {Marca} Modelo {Modelo} Dimensión {Dimensión} Tipo {TipoDeMonitor}";
     }
 }
 
-class Disco
+class Disco : ComponenteDeComputadora
 {
-    public string Tipo { get; set; }
+    public string TipoDeDisco { get; set; }
     public float FactorForma { get; set; }
     public int Capacidad { get; set; }
     public int Velocidad { get; set; }
@@ -60,8 +105,10 @@ class Disco
 
     public string ImprimirDisco()
     {
-        return $"Disco {Marca} Modelo {Modelo} Tipo {Tipo} Capacidad {Capacidad}";
+        return $"Disco {Marca} Modelo {Modelo} Tipo {TipoDeDisco} Capacidad {Capacidad}";
     }
 }
 
 // TODO: Si todo es un objeto en C# ¿Cuál es el objeto Padre de todos?
+// clase base
+// clase Object
